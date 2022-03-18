d3af91d7f5b6f55cae461d7d5b3d768f78ae0b41
jeremytanes <jeremy.tan.2019@scis.smu.edu.sg>
2022-03-18 22:23:47 +0800

    feat: swapped to dynamoose ORM instead of aws sdk

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const dynamoose = require("dynamoose");
+const ProductModel = require("./models/product");
 
-const client = new DynamoDBClient({
-  region: "ap-southeast-1",
-});
+const client = new DynamoDBClient();
 
 module.exports = { client };
